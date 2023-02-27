c4c010578f21ddff1587e1743d3853eee86ce316
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-02-27 21:49:54 +0300

    face detection in video

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2   
-img = cv2.imread("face.png")
+#img = cv2.imread("face.png")
+
+vid = cv2.VideoCapture("faces.mp4")
 cascade = cv2.CascadeClassifier("frontalface.xml")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face = cascade.detectMultiScale(gray, 1.3, 7)
-for (x,y,w,h) in face:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-cv2.imshow('face',img)
-cv2.waitKey(0)
+
+while 1:
+    a, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face = cascade.detectMultiScale(gray, 1.3, 7)
+    
+    for (x,y,w,h) in face:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+        cv2.imshow('face',frame)
+    if cv2.waitKey(5) & 0xFF == 'q':
+        break
+
+
+vid.release()
 cv2.destroyAllWindows()
