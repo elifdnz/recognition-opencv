df1753583dfe8a74ca18fa49ee79ced0828d4e1c
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-02-28 12:04:19 +0300

    Seperate folder

---
+++ body/body.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("body.png")
+
+body_cascade = cv2.CascadeClassifier("fullbody.xml")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+body = body_cascade.detectMultiScale(gray,1.3,4)
+
+for (x,y,w,h) in body:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+
+cv2.imshow("body",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
