8b8f065a56d4765a6246339c03e7f8ace036ce57
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-06 15:15:52 +0300

    add write to screen

---
+++ yazı/yazi.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as ny
+
+ekran = ny.ones((500,500,3),ny.uint8)*255
+
+text = "Deneme"
+
+color = (0,100,255)
+
+for i in range(7):
+    cv2.putText(ekran,text,(30,30+i*35),i,1,(0,100,255))
+
+cv2.imshow("Ekran",ekran)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
